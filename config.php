922554c3b47797df4cd55f84968f06a959bd008a
SteveR <136388557+stevie912@users.noreply.github.com>
2025-05-29 17:02:52 +0000

    DB connection created and tested with get_all_users function (per demo video).

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'r2okq.h.filess.io');
+define('DB_USER', 'cosc4806_plentynone');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', 'cosc4806_plentynone');  
+define('DB_PORT', '3305');
+
+?>
